d1b837ea9cef1a7c36dac52fa651641f1cb02a75
parbilla <parbilla@gmail.com>
2021-03-30 23:52:40 +0000

    init

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""create a unique FileStorage instance for your application"""
+"""Unique storage instance"""
 import os
-
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -9,7 +8,6 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-
 
 storage_type = os.getenv("HBNB_TYPE_STORAGE")
 
