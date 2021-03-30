d0550541bb138275fb56982f6598ca422b98e3a6
parbilla <parbilla@gmail.com>
2021-03-30 23:22:01 +0000

    city

--- models/city.py
+++ models/city.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, Integer, String, ForeignKey
+"""City Class"""
+import os
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
+
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey(states.id),nullable=False,)
-    name = Column(String(128), nullable=False)
+    """Defines City Class"""
+    if storage_type == "db":
+        __tablename__ = "cities"
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey(states.id), nullable=False,)
+        places = relationship('Place', backref='cities', cascade='delete')
+    else:
+        state_id = ''
+        name = ''
