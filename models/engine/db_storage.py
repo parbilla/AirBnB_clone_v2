98ab5c65c3fe1f4ca1eceb6e5769439d7ad723e9
parbilla <parbilla@gmail.com>
2021-03-31 00:14:42 +0000

    db

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""This is the database storage class for AirBnB"""
-import os
-
+"""Database storage Class for AirBnB"""
+from os import getenv
 from models.base_model import BaseModel, Base
 from models.user import User
 from models.state import State
@@ -9,8 +8,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
 
@@ -26,6 +24,7 @@ class DBStorage:
     __session = None
 
     def __init__(self):
+        """Creates the engine"""
         user = os.getenv("HBNB_MYSQL_USER")
         pwd = os.getenv("HBNB_MYSQL_PWD")
         host = os.getenv("HBNB_MYSQL_HOST")
@@ -38,8 +37,8 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
+        """Returns dictionary of all objects"""
         my_dict = {}
-
         if cls:
             for obj in self.__session.query(eval(cls)).all():
                 key = "{}.{}".format(obj.__class__.__name__, obj.id)
@@ -49,22 +48,23 @@ class DBStorage:
                 for obj in self.__session.query(subcls).all():
                     key = "{}.{}".format(obj.__class__.__name__, obj.id)
                     my_dict[key] = obj
-
         return my_dict
 
     def new(self, obj):
-        """ Set in obj as a database entry """
+        """Adds objects to current database session"""
         self.__session.add(obj)
 
     def save(self):
-        """ commit saves to database """
+        """Commit changes to the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
+        """Deletes from the current database session"""
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
+        """Creates all tables in the database"""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
@@ -72,6 +72,5 @@ class DBStorage:
         self.__session = Session()
 
     def close(self):
-        """ removes a private session attribute """
+        """Removes a private session attribute """
         self.__session.close()
-        
