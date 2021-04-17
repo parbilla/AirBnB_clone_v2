afb102fa2a826da746c5ebe2d7b24be3f10c5d4d
parbilla <parbilla@gmail.com>
2021-04-17 16:55:59 +0000

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
 
-sudo apt update
-sudo apt upgrade
-sudo apt -y install nginx
+IsNginx=$(which nginx)
+if [ -z $IsNginx ]; then
+    sudo apt-get update
+    sudo apt-get upgrade
+    sudo apt-get -y install nginx
+fi
+sudo ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/shared
 mkdir -p /data/web_static/releases/test
 echo "<html>
@@ -13,7 +17,10 @@ echo "<html>
     ¡Vamo La Cooperativa!
   </body>
 </html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
+if [ -e "/data/web_static/current" ]; then
+    rm /data/web_static/current
+fi
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 new_string="\\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n"
 sed -i "/server_name _/a $new_string" /etc/nginx/sites-available/default
