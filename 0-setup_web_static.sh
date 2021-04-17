1908760519e7f0b074fa77aa20c3583f381704cd
parbilla <parbilla@gmail.com>
2021-04-17 17:44:27 +0000

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -23,5 +23,5 @@ fi
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 new_string="\\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n"
-sed -i "/server_name _/a $new_string" /etc/nginx/sites-available/default
+sed -i "42i $new_string" /etc/nginx/sites-available/default
 sudo service nginx restart
