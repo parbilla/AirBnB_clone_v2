fa68cc720cad07027fe828ebba1f0ec3f37482ba
parbilla <parbilla@gmail.com>
2021-05-02 19:34:15 +0000

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,6 +14,7 @@ def states_list():
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def remove_Alchemy(exception):
     """Remove the current SQLAlchemy Session"""
